b329fa1e77016702eb95407669e89fd1d3fdacc2
Carlos Betancourt <146739071+DonBeta@users.noreply.github.com>
2024-09-05 18:03:07 +0000

    aumente el tamaño de la contraseña

--- src/api/models.py
+++ src/api/models.py
@@ -3,7 +3,7 @@ from . import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    password = db.Column(db.String(255), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
     contacts = db.relationship('Contact', backref='user', lazy=True)
 
