c48dbfec18ae7ddc0a193b55d1a71360b875a1cb
Carlos Betancourt <146739071+DonBeta@users.noreply.github.com>
2024-09-05 19:31:31 +0000

    cambio de uri

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mi_base_de_datos.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = 'clave_beta'  
-
-db = SQLAlchemy(app)
 jwt = JWTManager(app)
+db = SQLAlchemy(app)
+
 
 from . import routes, models
